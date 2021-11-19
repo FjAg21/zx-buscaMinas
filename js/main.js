8cb278697d20c065fffce915fb1f4049a2d647c1
fj08031965 <espaldamojada55@outlook.es>
2021-11-19 12:59:42 +0100

    Las celdas aparecen ordenadas y con su nombre, y el display de pedir datos desaparece.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
 
 
 
+
+class Tablero{
+
+  //const letras = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z'];
+
+  constructor() {
+
+  }
+
+
+  crearTablero(tablero, altura, anchura){
+
+    const letras = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z'];
+
+    for (let i = 1; i <= anchura; i++) {
+
+      const div = document.createElement('div');
+      tablero.appendChild(div);
+
+      for (let j = 1; j <= altura; j++) {
+
+        const button = document.createElement('button');
+        button.className = 'celdas';
+        button.type = 'button';
+        button.value = letras[i]+"" +j;
+        button.innerText = letras[i]+"" +j;
+        div.appendChild(button);
+
+      }
+    }
+
+  }// ----------- Fin crearTablero(.....)
+
+} // ----------- Fin Tablero{
+
+
+
 let crearTablero = document.getElementById('crearTablero');
 
 
@@ -11,28 +48,13 @@ document.getElementById('crearTablero').addEventListener( 'click', (e) => {
   let altura = document.getElementById('altura').value;
   let anchura = document.getElementById('anchura').value;
   let tablero = document.getElementById('tablero');
-  console.log(tablero)
+  let datosTablero = document.getElementById('datosTablero');
 
-  console.log(tablero)
+  datosTablero.style.display = 'none';// --- oculto el div pedir datos.
 
-  const numCasillas = altura * anchura;
-
-  console.log(numCasillas);
-
-
-  tablero.appendChild(table);
-
-  for (let i = 1; i <= numCasillas; i++) {
-
-    
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.innerText = i;
+  let table = new Tablero();
+  table.crearTablero(tablero, altura, anchura);
 
 
-    tablero.appendChild(button);
 
-
-  }
-
-});
+});// ----------- Fin document.getElementById('crearTablero').addEventListener
