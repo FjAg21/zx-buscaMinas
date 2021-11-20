d77edc2e16a533ee34b1c8a3f18280ee8ecca0e0
fj08031965 <espaldamojada55@outlook.es>
2021-11-20 10:12:48 +0100

    Optimizado mt: getCasillasAleatorias() para que no repita casillas repetidas.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ class Tablero{
 
   // --- para nombrar a las casillas.
   letras = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z'];
+
   // --- para almacenar tantos nº aleatorios como bombas elige el usuario.
   casillasAleatorias = [];
 
@@ -41,71 +42,10 @@ class Tablero{
     return this.totalCeldas;
   }
 
+  getNobreCasillas(){
 
-  // ---- crea un tablero en función de los parámetros que pase el usuario.
-  crearTablero(tablero, altura, anchura){
-
-    let cont = 0; // --- cuenta el nº de casillas que se van creando.
-    let contNumAleatorio = 0; // --- cuenta el total de nº de aleatorios.
-
-
-    let posicionBombas = this.getCasillasAleatorias(this.totalCeldas, this.bombas);
-
-
-    console.log('this.totalCeldas-- 00  ' +this.totalCeldas);//--------------------------------------------
-    console.log('this.bombas-- 00 ' +this.bombas);//--------------------------------------------
-    console.log('posicionBombas -Array -- 00 ' +posicionBombas) ;//--------------------------------------------
-    console.log('--------------------------------------------<br><br> <br>  '  ) ;
-
-
-    //let posicionBombas = this.getCasillasAleatorias(this.totalCeldas, this.bombas);
-
-    for (let i = 0; i <= anchura; i++) {
-
-      const div = document.createElement('div');// --- para cada valor de 'i' creo un div.
-      tablero.appendChild(div);
-
-      for (let j = 1; j <= altura; j++) {
-
-        const button = document.createElement('button');
-        button.className = 'celdas';
-        button.type = 'button';
-
-        console.log('posicionBombas[cont]-- 11  ' +posicionBombas[cont]);//--------------------------------------------
-        console.log('contNumAleatorio-------------------------------- 11 ' +contNumAleatorio);//--------------------------------------------
-
-
-        if (posicionBombas[cont] === contNumAleatorio){
-
-          console.log('posicionBombas[cont]-- 22  ' +posicionBombas[cont]);//--------------------------------------------
-          console.log('contNumAleatorio------------------------------- 22 ' +contNumAleatorio);//--------------------------------------------
-
-
-          button.innerHTML = "BB";
-          button.value  = "BB";
-
-          div.appendChild(button);
-          this.casillas[cont] = this.letras[i]+"" +j;// --- cargo el array con los valores asignado a cada botón.
-          cont++;
-
-        }
-        else {
-          button.innerText = this.letras[i]+"" +j;
-          button.value  = "BB";
-          div.appendChild(button);
-
-          this.casillas[cont] = this.letras[i]+"" +j;// --- cargo el array con los valores asignado a cada botón.
-
-        }
-
-        contNumAleatorio++;
-        console.log('contNumAleatorio contNumAleatorio ;  ' +contNumAleatorio );
-      }
-    }
-
-    console.log('num ale contNumAleatorio;  ' +contNumAleatorio );
-
-  }// ----------- Fin crearTablero(.....)
+    return this.casillas;
+  }
 
 
 
@@ -116,18 +56,72 @@ class Tablero{
   }// -----------Fin getValorCasillas()
 
 
-  // --- retorna un array con tantos nº aleatorios como 'numBombas', comprendidos entre: 0 y numCasillas.
+  // --- retorna un array ordenado con tantos nº aleatorios como 'numBombas', comprendidos entre: 0 y numCasillas.
   getCasillasAleatorias(numCasillas, numBombas){
 
-    for (let i = 0; i <numBombas; i++) {
-      let aleator = Math.round(Math.random()*numCasillas);
-      this.casillasAleatorias[i] = aleator;
+    let bombasAleatorias = [];
+    for (let i = 0; i < numCasillas; i++) {
+      bombasAleatorias[i] = i ;
+    }
+    
+    // -- desordeno el orden creado de las casillas del tablero.
+    bombasAleatorias.sort(() => Math.random() - 0.5);
 
-    }// ----------- Fin getCasillasAleatorias(numCasillas,
+    // -- cargo el atributo 'casillasAleatorias' con tantas posiciones de 'bombasAleatorias' como 'numBombas' haya.
+    for (let i = 0; i < numBombas; i++) {
+      this.casillasAleatorias[i] = bombasAleatorias[i];
+    }
 
-
+    // -- desordeno el atributo 'casillasAleatorias'
     return this.casillasAleatorias.sort( (a,b) => a - b);
-  }
+
+  }// ----------- Fin getCasillasAleatorias(numCasillas,
+
+
+  // ---- crea un tablero en función de los parámetros que pase el usuario.
+  crearTablero(tablero, altura, anchura){
+
+    let cont = 0; // --- cuenta el nº de casillas que se van creando.
+    let contNumAleatorio = 0; // --- cuenta el total de nº de aleatorios.
+    let posicionBombas = this.getCasillasAleatorias(this.totalCeldas, this.bombas);
+
+    console.log("--------------------------------------- " +posicionBombas);
+
+    for (let i = 0; i < anchura; i++) {
+
+      const div = document.createElement('div');// --- para cada valor de 'i' creo un div.
+      tablero.appendChild(div);
+
+      for (let j = 0; j < altura; j++) {
+
+        const button = document.createElement('button');
+        button.className = 'celdas';
+        button.type = 'button';
+
+        if (posicionBombas[cont] === contNumAleatorio){
+
+          button.innerHTML = "BB";
+          button.value  = "BB";
+
+          div.appendChild(button);
+          this.casillas[contNumAleatorio] = "BB";// --- cargo el array con los valores asignado a cada botón.
+          cont++;
+
+        }
+        else {
+          button.innerText = this.letras[i]+"" +j;
+          button.value  = "BB";
+          div.appendChild(button);
+
+          this.casillas[contNumAleatorio] = this.letras[i]+"" +j;// --- cargo el array con los valores asignado a cada botón.
+        }
+
+        contNumAleatorio++;
+      }
+    }
+
+  }// ----------- Fin crearTablero(.....)
+
 
 } // ----------- Fin class Tablero{
 
@@ -158,9 +152,11 @@ document.getElementById('crearTablero').addEventListener( 'click', (e) => {
   let bombasAleatoria = table.getCasillasAleatorias(totalCeldas, totalBombas);
 
 
-  console.log(table.getValorCasillas());//---------------------------------------------------------------
+  console.log('table.getValorCasillas: ' +table.getValorCasillas());//---------------------------------------------------------------
 
   console.log('Posición de la bombas: '+bombasAleatoria);//----------------------------------------------
+
+  console.log('getNobreCasillas(): ' +table.getNobreCasillas());//----------------------------------------------
 
 
 
