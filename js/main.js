d63a5986c39d76ba9b4eef0594f3ef92d975991a
fj08031965 <espaldamojada55@outlook.es>
2021-11-21 23:41:52 +0100

    Terminado, a falta de poner diseño CSS.

--- js/main.js
+++ js/main.js
@@ -10,8 +10,13 @@ class Tablero{
   // --- para almacenar tantos nº aleatorios como bombas elige el usuario.
   casillasAleatorias = [];
 
+  // almacena el nº de las celdas y las celdas con bomba, para compararlo con el array
+  // que tiene las celdas de alrrededor.
+  celdasNumeradas = [];
+
   casillas = []; // --- almacena el nombre de las casillas creadas.
 
+  posicionBombas = []; // --- almacena posición de las bombas al crear el tablero.
 
   constructor(altura, anchura, bombas) {
 
@@ -37,6 +42,11 @@ class Tablero{
     return this.bombas;
   }
 
+  getPosicionBombas(){
+
+    return this.posicionBombas;
+  }
+
   getTotalCeldas(){
 
     return this.totalCeldas;
@@ -46,7 +56,11 @@ class Tablero{
 
     return this.casillas;
   }
-  
+
+  getCeldasNumeradas(){
+
+    return this.celdasNumeradas;
+  }
 
   getValorCasillas(){// --------------------------- retorna un array con el valor que tiene cada uno de los valores.
 
@@ -75,38 +89,41 @@ class Tablero{
     return this.casillasAleatorias.sort( (a,b) => a - b);
 
   }// ----------- Fin getCasillasAleatorias(numCasillas,
-
+  
 
   // --- retorna un array con el valor de las celdas que están alrededor de la selecciona.
   getCeldasAlRededor(valorCelda, anchuraFila ){
 
-    let doce = (parseInt(valorCelda) - parseInt(anchuraFila));
-    let una = (parseInt(valorCelda) - parseInt(anchuraFila) + parseInt(1));
-    let tres = (parseInt(valorCelda) + parseInt(1));
-    let cinco = (parseInt(valorCelda) + parseInt(anchuraFila) + parseInt(1));
+    let celdasAlRededor = [];
+    let valorC = parseInt(valorCelda);
+    let anchuraF = parseInt(anchuraFila);
 
-    let seis = (parseInt(valorCelda) + parseInt(anchuraFila));
-    let siete = (parseInt(valorCelda) + parseInt(anchuraFila) - parseInt(1));
-    let nueve = (parseInt(valorCelda) + parseInt(-1));
-    let once = (parseInt(valorCelda) - parseInt(anchuraFila) - parseInt(1));
+    let doce = (valorC) - anchuraF;
+    let una = (valorC) - anchuraF + parseInt(1);
+    let tres = (valorC) + parseInt(1);
+    let cinco = (valorC) + anchuraF + parseInt(1);
 
-    let celdasAlRededor = [doce, una, tres, cinco, seis, siete, nueve, once ];
+    let seis = (valorC) + anchuraF;
+    let siete = (valorC) + anchuraF - parseInt(1);
+    let nueve = (valorC) - parseInt(1);
+    let once = (valorC) - anchuraF - parseInt(1);
 
+    celdasAlRededor = [doce, una, tres, cinco, seis, siete, nueve, once ];
     return celdasAlRededor;
+
   }
 
 
   // ---- crea un tablero en función de los parámetros que pase el usuario.
-  crearTablero(tablero, altura, anchura){
+  crearTableroSinMapa(tablero, altura, anchura){
 
     let cont = 0; // --- cuenta el nº de casillas que se van creando.
-    let cuentaCedasDesdeUno = 0;
+    let cuentaCeldasDesdeUno = 0;
 
     let contNumAleatorio = 0; // --- cuenta el total de nº de aleatorios.
-    let posicionBombas = this.getCasillasAleatorias(this.totalCeldas, this.bombas);
+    this.posicionBombas = this.getCasillasAleatorias(this.totalCeldas, this.bombas);
 
-
-    console.log("Situación de las bommbas: ----------- " +posicionBombas);//---------------------------------**********
+    console.log("Situación de las bommbas: ----------- " +this.posicionBombas);//---------------------------------**********
 
     for (let i = 0; i < anchura; i++) {
 
@@ -120,29 +137,83 @@ class Tablero{
         button.type = 'button';
 
         // --- 'posicionBombas[cont]' = celdas aleatoras. 'contNumAleatorio' = cada una de las celdas numeradas.
-        if (posicionBombas[cont] === contNumAleatorio){
+        if (this.posicionBombas[cont] === contNumAleatorio){
 
-          button.innerHTML = "BB";
+          button.innerHTML=  '&#9973' ;
           button.value  = "BB";
-          button.className = 'celdas';
+          button.className = 'celdas'
 
           div.appendChild(button);
           this.casillas[contNumAleatorio] = "BB";// --- cargo el array con los valores asignado a cada botón.
           cont++;
-
+          this.celdasNumeradas[contNumAleatorio] = contNumAleatorio; // todas las celdas con nº.
         }
         else {
-          button.innerText = this.letras[i]+"" +j;
+          button.innerHTML=  '&#9973' ;
 
-          button.value  = cuentaCedasDesdeUno;
+          button.value  = cuentaCeldasDesdeUno;
           div.appendChild(button);
           button.className = 'celdas';
 
           this.casillas[contNumAleatorio] = this.letras[i]+"" +j;// --- cargo el array con los valores asignado a cada botón.
+          this.celdasNumeradas[contNumAleatorio] = contNumAleatorio;  // todas las celdas con nº.
         }
 
         contNumAleatorio++;
-        cuentaCedasDesdeUno++;
+        cuentaCeldasDesdeUno++;
+      }
+    }
+
+  }// ----------- Fin crearTableroSinMapa(.....)
+
+
+  // ---- crea un tablero en función de los parámetros que pase el usuario.
+  crearTablero(tablero, altura, anchura){
+
+    let cont = 0; // --- cuenta el nº de casillas que se van creando.
+    let cuentaCeldasDesdeUno = 0;
+
+    let contNumAleatorio = 0; // --- cuenta el total de nº de aleatorios.
+    this.posicionBombas = this.getCasillasAleatorias(this.totalCeldas, this.bombas);
+
+    console.log("Situación de las bommbas: ----------- " +this.posicionBombas);//---------------------------------**********
+
+    for (let i = 0; i < anchura; i++) {
+
+      const div = document.createElement('div');// --- para cada valor de 'i' creo un div.
+      tablero.appendChild(div);
+
+      for (let j = 0; j < altura; j++) {
+
+        const button = document.createElement('button');
+        button.className = 'celdas';
+        button.type = 'button';
+
+        // --- 'posicionBombas[cont]' = celdas aleatoras. 'contNumAleatorio' = cada una de las celdas numeradas.
+        if (this.posicionBombas[cont] === contNumAleatorio){
+
+          button.innerHTML = "BB";
+          button.value  = "BB";
+          button.className = 'celdas'
+
+          div.appendChild(button);
+          this.casillas[contNumAleatorio] = "BB";// --- cargo el array con los valores asignado a cada botón.
+          cont++;
+          this.celdasNumeradas[contNumAleatorio] = contNumAleatorio; // todas las celdas con nº.
+        }
+        else {
+          button.innerText = this.letras[i]+"" +j;
+
+          button.value  = cuentaCeldasDesdeUno;
+          div.appendChild(button);
+          button.className = 'celdas';
+
+          this.casillas[contNumAleatorio] = this.letras[i]+"" +j;// --- cargo el array con los valores asignado a cada botón.
+          this.celdasNumeradas[contNumAleatorio] = contNumAleatorio;  // todas las celdas con nº.
+        }
+
+        contNumAleatorio++;
+        cuentaCeldasDesdeUno++;
       }
     }
 
@@ -168,59 +239,153 @@ document.getElementById('crearTablero').addEventListener( 'click', (e) => {
   datosTablero.style.display = 'none';// --- oculto el div pedir datos.
 
   let table = new Tablero(altura, anchura, numBombas);
-  let totalCeldas = table.getTotalCeldas();
-  let totalBombas = table.getBombas();
-
-
   table.crearTablero(tablero, altura, anchura);
-  let bombasAleatoria = table.getCasillasAleatorias(totalCeldas, totalBombas);
-  let casillas = table.getNobreCasillas();
-
-
-  //console.log('table.getValorCasillas: ' +table.getValorCasillas());//*******************--------------------------------------**********************----------
-
-  //console.log('getNobreCasillas(): ' +table.getNobreCasillas());//----------------------------------------------
 
   const celdas = document.querySelectorAll('.celdas');
 
 
+  let nombreDeCadaCelda = table.getNobreCasillas();
+  let numeroDeCadaCelda = table.getCeldasNumeradas();
+
+
+  //let totalCeldas = table.getTotalCeldas();
+  //let totalBombas = table.getBombas();
+
+  //let bombasAleatoria = table.getCasillasAleatorias(totalCeldas, totalBombas);
+  //let casillas = table.getNobreCasillas();
+
+
+  //console.log('table.getValorCasillas: ' +table.getValorCasillas());//*******************--------------------------------------**********************----------
+
+  console.log('Nombre de cada celda: ' +nombreDeCadaCelda);//----------------------------------------------
+  console.log("Número de cada celda: " +numeroDeCadaCelda)//--------------------------**********
 
   celdas.forEach(  celda => {
 
     // ===================================================================== CREO  EL EVENTO  PARA  EL  RATÓN-
     celda.addEventListener( 'mouseup', (e) => {
 
+      let valorDeLaCeldaSelecciona = celda.value;
+      let celdasAlRededorDeLaSeleccionada =
+                    table.getCeldasAlRededor(parseInt(valorDeLaCeldaSelecciona), parseInt(anchura) );
 
-      console.log("celda.value ++++++++++++" +celda.value);//*******************--------------------------------------**********************
-      console.log('table.getValorCasillas: ' +table.getValorCasillas());////*******************---------------
-      if (celda.value === 'BB'){
-        celda.style.backgroundColor = 'red';
-        celda.innerHTML = '<span style=\'font-size:16px;\'>&#x1F4A5;</</span>';
-        celdas[5].style.backgroundColor = '#E7EB4E';
+      //console.log("Valor de la celda seleccionada: ++++++++++++" +valorDeLaCeldaSelecciona);//*******************--------------------------------------**********************
+      console.log("Situación de las bommbas: ----------- " +table.posicionBombas);//---------------------------------******
+      console.log('celda.value  celda.value  ' +celda.value);//-------
 
-        //console.log('celdas[5] ' +celdas[5].value);//--------------------------------------------********************
-        //console.log('celdas[7] ' +celdas[7].value);//--------------------------------------------********************
-        console.log('celdas.length) ' +celdas.length);//--------------------------------------------********************
+      if (valorDeLaCeldaSelecciona !== 'BB') {
+        console.log("Valor de la celda seleccionada: ++++++++++++ " +valorDeLaCeldaSelecciona);//*******
+        console.log('Celdas A lRededor De La Celda Seleccionada ' +celdasAlRededorDeLaSeleccionada);//-------------------
+        let contadorBombasCercanas = 0;
+
+        for (let i = 0; i < table.posicionBombas.length; i++) {
+
+          for (let j = 0; j < celdasAlRededorDeLaSeleccionada.length; j++) {
+
+            if (table.posicionBombas[i] === celdasAlRededorDeLaSeleccionada[j]) {
+
+              contadorBombasCercanas++;
+            }
+
+            console.log('parseInt(table.getTotalCeldas()+1) ' +parseInt(table.getTotalCeldas()));
+            if (celdasAlRededorDeLaSeleccionada[j] >= 0 &&
+                        celdasAlRededorDeLaSeleccionada[j] < parseInt(table.getTotalCeldas())){
+
+              celdas[celdasAlRededorDeLaSeleccionada[j]].style.backgroundColor = '#E7EB4E';
+            }
+          }
+        }
+
+        celdas[valorDeLaCeldaSelecciona].innerHTML =
+                            '<span style=\'font-size:12px;\'>' + contadorBombasCercanas + '</span>';
+
+        celdas[valorDeLaCeldaSelecciona].style.backgroundColor = '#4eebd9';
       }
-      else{
-
-        let celdasAlRededor = table.getCeldasAlRededor(celda.value, anchura );
-
-        console.log('celdasAlRededor ------ celdasAlRededor ' +celdasAlRededor);//--------------------------------------------********************
-
-        celda.style.backgroundColor = '#ffffff';
-        celda.innerHTML = '<span style=\'font-size:16px;\'>&#x26F5;</</span>';
-        console.log('anchura ------ anchura ' +anchura);//--------------------------------------------********************
+      else {
+        celda.style.backgroundColor = '#f83f15';
       }
-      //celda.style.backgroundColor= 'yellow';
+
     });
-
   });
-
-
 });// ----------- Fin document.getElementById('crearTablero').addEventListener
 
 
 
+//********************************************************************************************************************
+
+
+// =========================================================== CREO  EL OBJETO  TABLERO SIN MAPA  CON UN EVENTO-
+
+
+document.getElementById('crearTableroSinMapa').addEventListener( 'click', (e) => {
+
+  let altura = document.getElementById('altura').value;
+  let anchura = document.getElementById('anchura').value;
+  let numBombas = document.getElementById('numMinas').value;
+
+  let tablero = document.getElementById('tablero');
+  let datosTablero = document.getElementById('datosTablero');
+  datosTablero.style.display = 'none';// --- oculto el div pedir datos.
+
+  let table = new Tablero(altura, anchura, numBombas);
+  table.crearTableroSinMapa(tablero, altura, anchura);
+
+  const celdas = document.querySelectorAll('.celdas');
+
+
+  // =================================================== CREO  EL EVENTO  PARA  EL  RATÓN-
+  celdas.forEach(  celda => {
+
+    celda.addEventListener( 'mouseup', (e) => {
+
+      let valorDeLaCeldaSelecciona = celda.value;
+      let celdasAlRededorDeLaSeleccionada =
+        table.getCeldasAlRededor(parseInt(valorDeLaCeldaSelecciona), parseInt(anchura) );
+
+      if (valorDeLaCeldaSelecciona !== 'BB') {
+
+        let contadorBombasCercanas = 0;
+
+        for (let i = 0; i < table.posicionBombas.length; i++) {
+
+          for (let j = 0; j < celdasAlRededorDeLaSeleccionada.length; j++) {
+
+            if (table.posicionBombas[i] === celdasAlRededorDeLaSeleccionada[j]) {
+
+              contadorBombasCercanas++;
+            }
+
+            console.log('parseInt(table.getTotalCeldas()+1) ' +parseInt(table.getTotalCeldas()));
+            if (celdasAlRededorDeLaSeleccionada[j] >= 0 &&
+              celdasAlRededorDeLaSeleccionada[j] < parseInt(table.getTotalCeldas())){
+
+              celdas[celdasAlRededorDeLaSeleccionada[j]].style.backgroundColor = '#E7EB4E';
+              //celdas[celdasAlRededorDeLaSeleccionada[j]].innerHTML = '&#127937;';
+            }
+          }
+        }
+
+        celdas[valorDeLaCeldaSelecciona].innerHTML =
+          '<span style=\'font-size:12px;\'>' + contadorBombasCercanas + '</span>';
+
+        celdas[valorDeLaCeldaSelecciona].style.backgroundColor = '#4eebd9';
+
+      }
+      else {
+
+        celdas.forEach( cel => {
+            cel.innerHTML = '&#128163;';
+            cel.style.backgroundColor = '#f83f15';
+
+        });
+      }
+    });
+
+  });
+
+});// ----------- Fin document.getElementById('crearTablero').addEventListener
+
+
+//https://www.youtube.com/watch?v=xwapo6FFhnQ
 
 
